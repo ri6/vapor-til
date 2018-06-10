670adf1299de48816320950996ac6bd4309e8d63
ri6 <ryani6@cloud.com>
2018-06-10 12:39:38 +1000

    removed double get router

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,19 +10,7 @@ public func routes(_ router: Router) throws {
     router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self).flatMap(to: Acronym.self) { acronym in
             return acronym.save(on: req)
-            
         }
-        
-        
     }
 
-    // 1
-    router.post("api", "acronyms") { req -> Future<Acronym> in
-  // 2
-  return try req.content.decode(Acronym.self)
-    .flatMap(to: Acronym.self) { acronym in
-    // 3
-    return acronym.save(on: req)
-        }
-    }
 }
